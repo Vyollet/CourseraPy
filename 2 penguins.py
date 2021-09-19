99959fbfc030e5eb8d2127d101e359d8f7d39a53
Ray <46301000+Vyollet@users.noreply.github.com>
2021-09-20 00:37:17 +0300

    -

--- 2 penguins.py
+++
@@ -1,6 +0,0 @@
-num = int(input())
-print(num * "   _~_    ")
-print(num * "  (o o)   ")
-print(num * " /  V  \  ")
-print(num * "/(  _  )\ ")
-print(num * "  ^^ ^^   ")
--- 3 apples.py
+++
@@ -1,2 +0,0 @@
-N, K = (int(input()) for _ in range(2))
-print(K // N)
